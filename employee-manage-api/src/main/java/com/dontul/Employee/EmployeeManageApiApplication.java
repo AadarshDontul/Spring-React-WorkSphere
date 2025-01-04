96c3c8ccefb136c1138f5892a656fe05246b1054
Aadarsh-D <aadarshdontul03@gmail.com>
2025-01-04 22:39:28 +0530

    Chnages

---
+++ employee-manage-api/src/main/java/com/dontul/Employee/EmployeeManageApiApplication.java
@@ -0,0 +1,13 @@
+package com.dontul.Employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManageApiApplication.class, args);
+	}
+
+}
---
+++ employee-manage-api/src/test/java/com/dontul/Employee/EmployeeManageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontul.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
